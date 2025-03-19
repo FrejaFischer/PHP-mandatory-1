f38fd01c16c5ac6a9e550936dffba80acf4ddf43
Freja Fischer <frej657e@stud.kea.dk>
2025-03-19 13:21:33 +0100

    converted all Employee methods to use Database execute methods

--- classes/Employee.php
+++ classes/Employee.php
@@ -33,26 +33,12 @@ Class Employee extends Database
     /**
      * It retrieves employee from the db based
      * on an ID
-     * @param $searchText The text to search for
-     * @return An associative array
+     * @param int $employeeID The employee to search for
+     * @return array if succes associative array
      *         Or false is there was an error
      */
     function getByID(int $employeeID): array|false
     {
-        // $sql =<<<SQL
-        //     SELECT 
-        //         employee.cFirstName AS first_name, 
-        //         employee.cLastName AS last_name, 
-        //         employee.cEmail AS email, 
-        //         employee.dBirth AS birth_date, 
-        //         employee.nDepartmentID AS department_id, 
-        //         department.cName AS department_name,
-        //         department.nDepartmentID AS department_ID
-        //     FROM employee INNER JOIN department
-        //         ON employee.nDepartmentID = department.nDepartmentID
-        //     WHERE nEmployeeID = :employeeID;
-        // SQL;
-
         return $this->executeSelect("SELECT 
                 employee.cFirstName AS first_name, 
                 employee.cLastName AS last_name, 
@@ -64,33 +50,12 @@ Class Employee extends Database
             FROM employee INNER JOIN department
                 ON employee.nDepartmentID = department.nDepartmentID
             WHERE nEmployeeID = ?;", [$employeeID]);
-        
-        // try{
-        //     $stmt = $this->pdo->prepare($sql);
-        //     // $stmt = $pdo->prepare($sql);
-            
-        //     $stmt->bindValue(':employeeID', $employeeID);
-        //     $stmt->execute();
-            
-        //     return $stmt->fetchAll();
-        // } catch(PDOException $e){
-        //     Logger::logText('Error getting all employee', $e);
-        //     return false;
-        // } catch(Exception $e){
-        //     Logger::logText('Error getting all employee', $e);
-        //     echo "An error occurred: " . $e->getMessage();
-        //     return false;
-        // } catch(Error $e){
-        //     Logger::logText('Error getting all employee', $e);
-        //     echo "A serious system error occurred: " . $e->getMessage();
-        //     return false;
-        // }
     }
 
     /**
-     * Retrieves employees from the db based
+     * Retrieves all employees from the db based
      * on a department ID
-     * @param $departmentID The department which employees should be from
+     * @param int departmentID The department which employees should be from
      * @return array of employees
      *         Or false is there was an error
      */
@@ -101,8 +66,8 @@ Class Employee extends Database
     
     /**
      * Retrieves employees from the db based
-     * on a department ID
-     * @param $departmentID The department which employees should be from
+     * on a project ID
+     * @param int $projectID The project which employees should be from
      * @return array of employees
      *         Or false is there was an error
      */
@@ -116,42 +81,19 @@ Class Employee extends Database
     
     /**
      * It inserts an employee into the db
-     * @param $pdo A PDO database connnection
-     * @param $employee The employee to insert
+     * @param array $employee The employee to insert
      * @return boolean, true if success, false if error
      */
     function insert(array $employee): bool
     {
-        // $pdo = $this->connect();
-        $sql =<<<SQL
-            INSERT INTO `employee`(`cFirstName`, `cLastName`, `cEmail`, `dBirth`, `nDepartmentID`) 
-            VALUES (?, ?, ?, ?, ?);
-        SQL;
-        
-        try{
-            $firstName = htmlspecialchars(trim($employee['firstName']));
-            $lastName = htmlspecialchars(trim($employee['lastName']));
-            $email = htmlspecialchars(trim($employee['email']));
-            $birth = htmlspecialchars(trim($employee['birth']));
-            $departmentID = htmlspecialchars(trim($employee['department']));
-            
-            $stmt = $this->pdo->prepare($sql);
-            // $stmt = $pdo->prepare($sql);
-            $stmt->execute([
-                $firstName, 
-                $lastName, 
-                $email, 
-                $birth, 
-                $departmentID
-            ]);
-            
-            return $stmt->rowCount() === 1;
-            
-            
-        } catch(PDOException $e){
-            Logger::logText('Error inserting employee', $e, 'Function:'.__FUNCTION__ ,'Line number:'.__LINE__);
-            return false;
-        }
+        $firstName = htmlspecialchars(trim($employee['firstName']));
+        $lastName = htmlspecialchars(trim($employee['lastName']));
+        $email = htmlspecialchars(trim($employee['email']));
+        $birth = htmlspecialchars(trim($employee['birth']));
+        $departmentID = htmlspecialchars(trim($employee['department']));
+
+        return $this->executeQuery("INSERT INTO `employee`(`cFirstName`, `cLastName`, `cEmail`, `dBirth`, `nDepartmentID`) 
+            VALUES (?, ?, ?, ?, ?);",[$firstName, $lastName, $email, $birth, $departmentID]);
     }
     
     /**
@@ -214,71 +156,30 @@ Class Employee extends Database
     
     /**
      * It updates an employee in the db
-     * @param $pdo A PDO database connnection
-     * @param $employee The employee to update
-     * @param $employeeID The employees id
+     * @param array $employee The employee info to update
+     * @param int $employeeID The employees id
      * @return boolean, true if success, false if error
      */
     function update(array $employee, int $employeeID): bool
     {
-        // $pdo = $this->connect();
-        $sql =<<<SQL
-            UPDATE `employee` SET `cFirstName` = :firstName, `cLastName` = :lastName, `cEmail` = :email, `dBirth` = :birthDate, `nDepartmentID` = :departmentID
-            WHERE nEmployeeID = :employeeID
-        SQL;
-        try{
-            $firstName = htmlspecialchars(trim($employee['firstName']));
-            $lastName = htmlspecialchars(trim($employee['lastName']));
-            $email = htmlspecialchars(trim($employee['email']));
-            $birth = htmlspecialchars(trim($employee['birth']));
-            $departmentID = htmlspecialchars(trim($employee['department']));
-            
-            $stmt = $this->pdo->prepare($sql);
-            // $stmt = $pdo->prepare($sql);
-            
-            $stmt->bindValue(':firstName', $firstName);
-            $stmt->bindValue(':lastName', $lastName);
-            $stmt->bindValue(':email', $email);
-            $stmt->bindValue(':birthDate', $birth);
-            $stmt->bindValue(':departmentID', $departmentID);
-            $stmt->bindValue(':employeeID', $employeeID);
-            $stmt->execute();
-            
-            return $stmt->rowCount() === 1;
-            
-            
-        } catch(PDOException $e){
-            Logger::logText('Error updating employee', $e);
-            return false;
-        }
+        $firstName = htmlspecialchars(trim($employee['firstName']));
+        $lastName = htmlspecialchars(trim($employee['lastName']));
+        $email = htmlspecialchars(trim($employee['email']));
+        $birth = htmlspecialchars(trim($employee['birth']));
+        $departmentID = htmlspecialchars(trim($employee['department']));
+
+        return $this->executeQuery("UPDATE `employee` SET `cFirstName` = ?, `cLastName` = ?, `cEmail` = ?, `dBirth` = ?, `nDepartmentID` = ?
+            WHERE nEmployeeID = ?", [$firstName, $lastName, $email, $birth, $departmentID, $employeeID]);
     }
     
     /**
      * It deletes an employee in the db
-     * @param $pdo A PDO database connnection
-     * @param $employeeID The employees id
+     * @param $employeeID The employee to delete id
      * @return boolean, true if success, false if error
      */
     function delete(int $employeeID): bool
     {
-        // $pdo = $this->connect();
-        $sql =<<<SQL
-            DELETE FROM `employee` WHERE nEmployeeID = :employeeID
-        SQL;
-    
-        try{
-            $stmt = $this->pdo->prepare($sql);
-            // $stmt = $pdo->prepare($sql);
-            $stmt->bindValue(':employeeID', $employeeID);
-            $stmt->execute();
-            
-            return $stmt->rowCount() === 1;
-            
-            
-        } catch(PDOException $e){
-            Logger::logText('Error deleting employee', $e);
-            return false;
-        }
+        return $this->executeQuery("DELETE FROM `employee` WHERE nEmployeeID = ?", [$employeeID]);
     }
 
     // when object gets echo, this is called
