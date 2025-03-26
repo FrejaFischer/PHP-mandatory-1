eea630ce47bf472631a625224905700757fb48e2
Freja Fischer <frej657e@stud.kea.dk>
2025-03-26 15:17:58 +0100

    styled landing page

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ include_once ROOT_PATH . '/public/header.php';
 
 ?>
     <main>
+        <img src=<?=BASE_URL . '/assets/work_image.jpeg'?> alt="Our workspace">
         <p>At the forefront of progress, we shape possibilities into realities through a seamless fusion of expertise, adaptability, and forward-thinking solutions. Driven by an unwavering commitment to excellence, we navigate complexity with precision, delivering results that redefine expectations. Whether optimizing existing frameworks or pioneering new frontiers, our approach remains dynamic, innovative, and tailored to the ever-evolving landscape.</p>
     </main>
 <?php include_once ROOT_PATH . '/public/footer.php'; ?>
